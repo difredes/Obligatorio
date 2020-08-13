324a51789173bf3752957220c7ba7251bbe55694
Diego <difredespelletti@gmail.com>
2020-08-13 19:51:37 -0300

    Se eliminaron comentarios
    -

--- js/products.js
+++ js/products.js
@@ -29,9 +29,7 @@ function showCategoriesList(array){
 }
 
 
-//Función que se ejecuta una vez que se haya lanzado el evento de
-//que el documento se encuentra cargado, es decir, se encuentran todos los
-//elementos HTML presentes.
+
 document.addEventListener("DOMContentLoaded", function(e){
     getJSONData(PRODUCTS_URL).then(function(resultObj){
         if (resultObj.status === "ok")
