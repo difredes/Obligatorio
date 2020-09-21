5f14337b3a32ec5557314a78b5ee8c51a6e90a2f
Diego <difredespelletti@gmail.com>
2020-09-20 23:19:03 -0300

    Entregable 04

--- js/product-info.js
+++ js/product-info.js
@@ -1,28 +1,48 @@
-//Entrega 3
-var product = {};
-var commentsArray = [];
-const now = new Date();
+//Función que se ejecuta una vez que se haya lanzado el evento de
+//que el documento se encuentra cargado, es decir, se encuentran todos los
+//elementos HTML presentes.
+
+var producto = {};
+
+function showrelatedProducts(arraylistado,arrayrelacionados){
+    let contenido ="<hr>"
+    arrayrelacionados.forEach(function(i){
+        
+        contenido +=`
+        
+        <p  class=" list-group-item-action">
+        
+            <div class="row">
+                
+                <div class="col">
+                    <div class="d-flex w-100 justify-content-between">
+                        <dt class="mb-1">`+  arraylistado[i].name+`</dt>
+                        
+                    </div>
+                    <p class="mb-1">` + "Precio:"+" "+arraylistado[i].currency + arraylistado[i].cost  + `</p>
+                    <img src="${arraylistado[i].imgSrc}"width="235">;<br>
+        <a href="product-info.html"> <button style="float: right;"> Ver producto </button></a>
+                   
+                </div>
+            </div>
+        </p>
+        `
+    });
+    document.getElementById("relatedProducts").innerHTML = contenido ;
+}
+        
 
 
 
-function showProduct(product, arrayComments) {
-    let imagesArray = product.images;
-    let comentarios = "";
-    let imgs = "";
-    let productNameHTML = document.getElementById("productName");
-    let productDescriptionHTML = document.getElementById("productDescription");
-    let productCostHTML = document.getElementById("productCost");
-    let productCountHTML = document.getElementById("soldCount");
 
-    productNameHTML.innerHTML = product.name;
-    productDescriptionHTML.innerHTML = product.description;
-    productCostHTML.innerHTML = product.cost + " " + product.currency;
-    productCountHTML.innerHTML = product.soldCount + " " + "unidades";
+function showImages(array){
 
-    for (let i = 0; i < imagesArray.length; i++) {
-        let imageSrc = imagesArray[i];
+    let htmlContentToAppend = "";
 
-        imgs += `
+    for(let i = 0; i < array.length; i++){
+        let imageSrc = array[i];
+
+        htmlContentToAppend += `
         <div class="col-lg-3 col-md-4 col-6">
             <div class="d-block mb-4 h-100">
                 <img class="img-fluid img-thumbnail" src="` + imageSrc + `" alt="">
@@ -30,81 +50,114 @@ function showProduct(product, arrayComments) {
         </div>
         `
 
-        document.getElementById("productImagesGallery").innerHTML = imgs;
-
-
+        document.getElementById("productImages").innerHTML = htmlContentToAppend;
     }
-
-    for (let comment in arrayComments){
-        auxComment = arrayComments[comment];
-        let score = "";
-
-        for(let i=1; i<=auxComment.score; i++){
-            score += `<span class="fa fa-star checked"></span>`;
-        }
-
-        for(let i=auxComment.score + 1; i<=5; i++){
-            score += `<span class="fa fa-star"></span>`;
-        }
-
-        comentarios += `
-                <div class="list-group-item list-group-item-action">
-                            <div class="d-flex w-100 justify-content-between">
-                            <div class="mb-1">
-                                <h4>`+ auxComment.user + '  ' + score + `</h4>
-                             <p>`+ auxComment.description + `</p>
-                         </div>    
-                             <small class="text-muted">` + auxComment.dateTime.split(' ')[0] + `</small>
-                          </div>
-
-                      </div>
-                   </div>
-               </div>
-              `
-        document.getElementById("cat-list-container").innerHTML = comentarios;
-        
-    
-    }
-
-    
 }
 
+function showcomments(arrayComments){
+
+    let comments= "";
 
 
-//Función que se ejecuta una vez que se haya lanzado el evento de
-//que el documento se encuentra cargado, es decir, se encuentran todos los
-//elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function (e) {
-    getJSONData(PRODUCT_INFO_URL).then(function (resultObj) {
-        if (resultObj.status === "ok") {
 
+    for(comment in arrayComments){
+        let puntos = "";
+        
+
+        for (let i =1 ; i<= arrayComments[comment].score; i++)    {
+            puntos +=  ` <span class="fa fa-star checked"></span> `;
+        }    
+        for (let i = arrayComments[comment].score + 1; i<=5; i ++)    {
+            puntos +=  ` <span class="fa fa-star"></span> `;
+            }
+
+        comments += `
+        <a  class="list-group-item list-group-item-action">
+            <div class="row">
+                
+                <div class="col">
+                    <div class="d-flex w-100 justify-content-between">
+                        <dt class="mb-1">`+ arrayComments[comment].user+`</dt>
+                        <small class="text-muted">` + arrayComments[comment].dateTime + ` </small>
+                    </div>
+                    <p class="mb-1">` +arrayComments[comment].description  + `</p>
+                    <p class="mb-1">` + puntos + `</p>
+                </div>
+            </div>
+        </a>
+        `
+        
+      
+        }
+
+
+        document.getElementById("productcoment").innerHTML = comments ;
+    }
+
+
+
+document.addEventListener("DOMContentLoaded", function(e){
+    getJSONData(PRODUCT_INFO_URL).then(function(resultObj){
+        if (resultObj.status === "ok")
+        {
             product = resultObj.data;
 
+            let productNameHTML  = document.getElementById("productName");
+            let productDescriptionHTML = document.getElementById("productDescription");
+            let productCostHTML = document.getElementById("productCount");
+            let productCrHTML = document.getElementById("productCriteria");
+        let productSoldHTML = document.getElementById("productSold");
+        let productCatHTML = document.getElementById("productCat")
+
+           productNameHTML.innerHTML = product.name;
+            productDescriptionHTML.innerHTML = product.description;
+            productCostHTML.innerHTML = product.cost;
+            productCrHTML.innerHTML = product.currency;
+
+            productSoldHTML.innerHTML = product.soldCount;
+            productCatHTML.innerHTML = product.category;
+
+            //Muestro las imagenes en forma de galería
+            showImages(product.images);
         }
     });
-
-    getJSONData(PRODUCT_INFO_COMMENTS_URL).then(function (resultObj) {
-        if (resultObj.status === "ok") {
-
-            commentsArray = resultObj.data;
-
-            showProduct(product, commentsArray);
-        }
-    }); 
-    //Desafiate 
-    document.getElementById("enviarComm").addEventListener("click", function() {
-
-        let newComment = {
-
-            score : parseInt(document.getElementById('newCal').value),
-            description : document.getElementById('newComm').value,
-            user : JSON.parse(localStorage.getItem('User-Logged')).email,
-            dateTime : "2020-02-25 18:03:52"
-        };
-
-        commentsArray.push(newComment);
-        showProduct(product, commentsArray);
-    })
-
 });
 
+
+
+
+
+
+
+
+
+    document.addEventListener("DOMContentLoaded", function (e) {
+
+
+
+        getJSONData(PRODUCT_INFO_COMMENTS_URL).then(function(resultObj){
+            if (resultObj.status === "ok"){
+                comment = resultObj.data;
+               showcomments(comment);
+            }
+        });
+
+        getJSONData(PRODUCT_INFO_URL).then(function(resultObj) {
+            if (resultObj.status === "ok") {
+                product = resultObj.data;
+
+                
+            }});
+
+
+            getJSONData(PRODUCTS_URL ).then(function(resultObj) {
+                if (resultObj.status === "ok") {
+                    productarray = resultObj.data;
+                    showrelatedProducts(productarray,product.relatedProducts);
+                    
+    
+                    
+                }});
+    
+          
+        });
