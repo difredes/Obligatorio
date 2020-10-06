1018510a3ba792dd54ee84811e99289c50105ffe
Diego <difredespelletti@gmail.com>
2020-10-06 19:09:45 -0300

    Entrega 5
    Cambios de Funcionamiento

--- js/cart.js
+++ js/cart.js
@@ -1,67 +1,157 @@
-var cartProduct = []
+let articulos = {};
+let articulos2 = {};
+let precioUnit = 0;
+let precioUnit2 = 0;
+let num = 0;
+let num2 = 0;
+let impSubtotal = 0;
+let impSub2 = 0;
+
+let subT = 0;
+var subT2 = 0;
+var porcentajeEnvio = 0.15;
 
 
-// Función para mostrar producto en el carrito
-function showCartProducts() {
 
-    let htmlContentToAppend = "";
 
-    console.log(cartProduct);
-    console.log(cartProduct.length);
-    for (let i = 0; i < cartProduct.length; i++) {
-        let product = cartProduct[i];
+function actualizarCostos() {
 
-        var subTotal = product.unitCost * product.count;
+    let subText = document.getElementById("subtotalText");
+    let envText = document.getElementById("envioText");
+    let totalText = document.getElementById("totalCostText");
 
-        htmlContentToAppend += `                  
-        <tr class="justify-content-md-center">
-        <th scope="row text-"><img src="` + product.src + `" class="w-25" alt="">  
-        </th>
-        <td class="text-center">` + product.name + `</td>
-        <td id="prodUnitCost" class="text-center">` + product.unitCost + `</td>
-        <td class="text-center">
-        <input onchange="subTotalByCount()" id="prodCartCount" class="text-center w-50" type="number" placeholder="` + product.count + `">
-        </td>
-        <td class="text-center">$<span id="subTotalCart">`+ subTotal + `</span></td>
-        </tr>
-        `
-        ;
-    }
-    document.getElementById("cart-product").innerHTML = htmlContentToAppend;
-    
+    //calculo el precio unitario del pino
+    var t1 = (subT * 100);
+    //calculo el precio unitario de autos y lo paso a pesos 
+    var t2 = (subT2 * 12500 * 40);
+    var t = (t2 + t1);
+
+    let subtextHTML = t;
+    let costoEnvioHTML = (porcentajeEnvio * t);
+    let totalEnvioHTML = Math.round(t + costoEnvioHTML);
+
+    subText.innerHTML = subtextHTML;
+    envText.innerHTML = costoEnvioHTML;
+    totalText.innerHTML = totalEnvioHTML;
+
+
+    let detalleHtml = document.getElementById("detalleCompra");
+    detalleHtml.innerHTML = ` Total a pagar $ ` + totalEnvioHTML;
+
+
 }
 
-// Función para calcular subtotal = costo unitario * cantidad (x)
-function subTotalByCount() {
 
-    let i = 0;
-    let product = cartProduct[i];
 
-    var x = document.getElementById("prodCartCount").value;
-    document.getElementById("subTotalCart").innerHTML = product.unitCost * x;
+//funcion que se ejecuta cuando se hace click en el boton aceptar
+function btnAceptar() {
+
+    document.getElementById("calle").value="";
+    document.getElementById("numeroL").value="";
+    document.getElementById("esquina").value ="";
+    document.getElementById("detalleCompra").innerHTML = '';
+    document.getElementById("subtotalText").innerHTML = 0;
+    document.getElementById("envioText").innerHTML = 0;
+    document.getElementById("totalCostText").innerHTML = 0;
+    document.getElementById("numero").value = 0;
+    document.getElementById("numero2").value = 0;
+    document.getElementById("subTotal").innerHTML = 0;
+    document.getElementById("subTotal2").innerHTML = 0;
+
+
+
 }
 
 
-//Función que se ejecuta una vez que se haya lanzado el evento de
-//que el documento se encuentra cargado, es decir, se encuentran todos los
-//elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function (e) {
-    getJSONData(CART_INFO_URL).then(function (restultObj) {
-        if (restultObj.status === "ok") {
-            cartProduct = restultObj.data.articles;
+
+
+    //funcion que trae el numero del input, se lo paso a una variable  e imprimo en otro id el monto.
+    document.getElementById("numero").addEventListener("change", function () {
+        var num = document.getElementById("numero").value;
+
+        impSubtotal = document.getElementById("subTotal");
+        impSubtotal.innerHTML = num * 100;
+        subT = 100 * num;
+        subT = this.value
+
+        actualizarCostos();
+
+    });
+
+    //funcion que trae el numero del input, se lo paso a una variable  e imprimo en otro id el monto.
+    document.getElementById("numero2").addEventListener("change", function () {
+        var num2 = document.getElementById("numero2").value;
+
+        impSub2 = document.getElementById("subTotal2");
+        impSub2.innerHTML = (40 * 12500) * num2;
+        subT2 = (Math.round(12500 * 40) * impSub2);
+
+        subT2 = this.value;
+        actualizarCostos();
+
+    });
+
+
+    document.getElementById("premiumradio").addEventListener("change", function () {
+        porcentajeEnvio = 0.15;
+        actualizarCostos()
+    });
+
+    document.getElementById("expressradio").addEventListener("change", function () {
+        porcentajeEnvio = 0.07;
+        actualizarCostos()
+    });
+
+    document.getElementById("standardradio").addEventListener("change", function () {
+        porcentajeEnvio = 0.05;
+        actualizarCostos()
+    });
+
+
+
+
+
+
+
+
+});
+
+
+
+
+
+
+
+
+
+document.addEventListener("DOMContentLoaded", function (ex) {
+    getJSONData(ARTICLES_CART).then(function (res) {
+        if (res.status === "ok") {
+            art = res.data;
+
+            articulos = document.getElementById("art");
+            articulos2 = document.getElementById("art2");
+            precioUnit = document.getElementById("costoUnit");
+            precioUnit2 = document.getElementById("costoUnit2");
+            num = document.getElementById("numero");
+            num2 = document.getElementById("numero2");
+
+            for (let i = 0; i < art.articles.length; i++) {
+                articulos.innerHTML = art.articles[0].name;
+                precioUnit.innerHTML = art.articles[0].currency + ` ` + art.articles[0].unitCost;
+                num.value = art.articles[0].count;
+
+                if (art.articles[i]) {
+
+                    articulos2.innerHTML = art.articles[i].name;
+                    precioUnit2.innerHTML = art.articles[i].currency + ` ` + art.articles[i].unitCost;
+                    num2.value = art.articles[i].count;
+                }
+            }
+
         }
 
-        console.log(cartProduct);
 
-        showCartProducts();
     })
-
-    // document.getElementById("prod-cart-count").addEventListener("onchange", function () {
-
-    //     function subTotal() {
-
-    //         var x = getElementsByClassName("prod-cart-count").value;
-    //         document.getElementById("sub-total-cart").innerHTML = subTotal * x;
-    //     }
-    // })
 });
